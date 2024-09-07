17a1e84ad5933792c8cedc4b5780a52cc6bd8e9b
郁弥 田村 <k021g1134@m.neec.ac.jp>
2024-09-07 23:48:44 +0900

    パーティクルがでないように

--- Engine/2d/ParticleManager2D.cpp
+++ Engine/2d/ParticleManager2D.cpp
@@ -11,18 +11,11 @@ ParticleManager2D* ParticleManager2D::GetInstance()
 
 void ParticleManager2D::Initialize()
 {
-	emitter_.emplace_back();
-	emitter_.back().Initialize();
-	emitter_.back().SetPosition({ 500.0f, 500.0f });
-	emitter_.back().SetTextureHandle(LoadTexture("hae.png"));
-	//Vector2 genPos = { Util::GetRandomFloat(-100.0f, 100.0f), Util::GetRandomFloat(-100.0f, 100.0f) };
-	//emitter_.back().AddParticle(genPos, 60.0f, 1.0f, 0.0f);
+
 }
 
 void ParticleManager2D::Update()
 {
-	Vector2 genPos = { Util::GetRandomFloat(-100.0f, 100.0f), Util::GetRandomFloat(-100.0f, 100.0f) };
-	emitter_.back().AddParticle(genPos, 2.0f, 100.0f, 0.0f);
 	for (auto& it : emitter_) it->Update();
 	for (auto& it : emitter_) it->ImGuiUpdate();
 }
