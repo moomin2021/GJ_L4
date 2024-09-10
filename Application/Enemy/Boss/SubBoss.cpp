055c7abaaa45a88955966e68ec8715bc6c0ff2b3
郁弥 田村 <k021g1134@m.neec.ac.jp>
2024-09-10 17:40:54 +0900

    サブボスの描画

--- Application/Enemy/Boss/SubBoss.cpp
+++ Application/Enemy/Boss/SubBoss.cpp
@@ -1,5 +1,9 @@
 #include "SubBoss.h"
+#include "Texture.h"
+
 #include "Player.h"
+
+SubBoss::SubBoss() : subBossTextures_(3) {}
 
 void SubBoss::Initialize(M_ColliderManager* colMgrPtr, Player* playerPtr)
 {
@@ -7,6 +11,12 @@ void SubBoss::Initialize(M_ColliderManager* colMgrPtr, Player* playerPtr)
 	pPlayer_ = playerPtr;
 	colMgrPtr = colMgrPtr;
 
+	// サブボスの描画関連
+	subBossSprite_ = std::make_unique<Sprite>();
+	subBossSprite_->SetPosition(subBossPos_);
+	subBossSprite_->SetSize(subBossSize_);
+	subBossSprite_->SetAnchorPoint({ 0.5f, 0.5f });
+	subBossTextures_ = LoadDivTexture("subBossSheet.png", 3);
 }
 
 void SubBoss::Update()
@@ -15,10 +25,12 @@ void SubBoss::Update()
 
 void SubBoss::MatUpdate()
 {
+	subBossSprite_->MatUpdate();
 }
 
 void SubBoss::Draw()
 {
+	subBossSprite_->Draw(subBossTextures_[0]);
 }
 
 void SubBoss::Finalize()
--- Application/Enemy/Boss/SubBoss.h
+++ Application/Enemy/Boss/SubBoss.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "M_ColliderManager.h"
+#include "Sprite.h"
+
+#include <memory>
 
 class Player;
 class SubBoss
@@ -8,10 +11,22 @@ class SubBoss
 private:
 	// プレイヤーのポインタ
 	Player* pPlayer_ = nullptr;
+
+	// サブボスのデータ
+	Vector2 subBossPos_ = Vector2(960.0f, 400.0f);
+	Vector2 subBossSize_ = Vector2(256.0f, 256.0f);
+
+	// サブボスの描画関連
+	std::unique_ptr<Sprite> subBossSprite_ = nullptr;
+	std::vector<uint16_t> subBossTextures_;
 #pragma endregion
 
 #pragma region メンバ関数
 public:
+	// コンストラクタとデストラクタ
+	SubBoss();
+	~SubBoss() {}
+
 	// 基本処理
 	void Initialize(M_ColliderManager* colMgrPtr, Player* playerPtr);
 	void Update();
