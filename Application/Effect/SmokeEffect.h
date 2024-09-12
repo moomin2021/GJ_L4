aa51ffad3cc6a47f837488b6b3604c4058a2cabe
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2024-09-13 04:25:51 +0900

    煙エフェクト途中、描画が確認できず。

---
+++ Application/Effect/SmokeEffect.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ParticleMan.h"
+#include "AppEasing.h"
+
+class SmokeEffect:
+	public Particle2D
+{
+public:
+	SmokeEffect(void) = default;
+	~SmokeEffect(void)override = default;
+
+	void Initialize(void)override;
+
+	void Update(void)override;
+
+private:// メンバー変数
+	AppEasing mEaseRota;
+};
+
