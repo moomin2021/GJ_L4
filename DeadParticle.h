0d8594d1bf38c0ad093b77ee300f14a3208de016
maru0522 <94513876+maru0522@users.noreply.github.com>
2024-09-13 15:57:30 +0900

    sibouhosi

---
+++ DeadParticle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ParticleMan.h"
+class DeadParticle : public Particle2D
+{
+public:
+    DeadParticle(void) = default;
+    ~DeadParticle(void) override = default;
+
+    void Initialize(void) override;
+    void Update(void) override;
+};
+
